fa63cc7f30c80ee1a585f78f4267bed78f8a5f29
Davron <ihmatullaev@gmail.com>
2024-02-09 18:45:55 +0500

    few shot learning

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,16 +1,19 @@
 from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.types import Message
-import re
+import os
 import logging
 from environs import Env
+import asyncio
 
 from aiogram import flags
 from aiogram.fsm.context import FSMContext
-
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, FSInputFile, KeyboardButton, ReplyKeyboardMarkup
 import utils
+import message_texts
 from states import Gen
 import loader
+
 
 router = Router()
 bot = loader.bot
@@ -20,7 +23,7 @@ env = Env()
 env.read_env()#'../.env', recurse=False)
 
 
-# Handler for start command at the beginning
+# Обработчик для команды Старт
 @router.message(Command("start"))
 async def start_handler(msg: Message, state: FSMContext):
     """
@@ -29,65 +32,161 @@ async def start_handler(msg: Message, state: FSMContext):
     await utils.insert_user(msg.from_user.id, msg.from_user.username)
     conversation = await utils.get_user_conversation(msg.from_user.id)
     input = 'Здравствуйте!'
+    logging.info(f"Обработка запроса пользователя {msg.from_user.id}. Запрос: {input}")
     answer = await utils.get_answer_from_llm(conversation, input)
+    logging.info(f"Ответ на запрос пользователя {msg.from_user.id}. Ответ от ИИ: {answer}")
     # Сохраняем сообщения в базе
     await utils.insert_chat(msg.from_user.id, input=input, output=answer)
-    await msg.answer(answer
-    )  # Greeting message to user
-    await state.set_state(Gen.chat_state)  # change State to Initial State
+    is_admin = await utils.is_admin(user_id=msg.from_user.id)
+    if is_admin:        
+        admin_kb = [[KeyboardButton(text=message_texts.commands['report'], callback_data="get_report")], 
+                    [KeyboardButton(text=message_texts.commands['delete_context'], callback_data="delete_context")]]
+        admin_kb = ReplyKeyboardMarkup(keyboard=admin_kb, resize_keyboard=True)
+            # Вывод ответа администратору
+        await msg.answer(
+        answer, reply_markup=admin_kb
+    ) 
+    else:
+        # Вывод ответа обчному пользователю
+        await msg.answer(answer)  
+    await state.set_state(Gen.chat_state)  # change State to Chat State
+   
     # logging.info(convesations)
-    logging.info(input)
-    logging.info(answer)
+    # logging.info(input)
+    # logging.info(answer)
 
 
 
-# Catch  "Questions" from client #Gen.chat_state
-@router.message(F.content_type == 'text')
+# Колбэк для отправки файла отчета по сообщениям
+@router.message(F.text == message_texts.commands['report'])
+async def get_chats_report(msg: Message):
+    is_admin = await utils.is_admin(user_id=msg.from_user.id)
+    if is_admin: 
+        report_status = await utils.create_chats_report()
+        if report_status:
+            try:
+                if os.path.exists('docs/chat_history.xlsx'):
+                    doc_path = 'docs/chat_history.xlsx'
+                    await msg.answer_document(FSInputFile(doc_path))
+                    logging.info(f"Отчёт по чатам отправлен пользователю {msg.chat.id}")
+                else:
+                    await msg.answer("Файл отчета не найден")
+            except Exception as e:
+                logging.error(e)
+        else:
+            await msg.answer("Файл отчёта не создан()")
+    else:
+        await msg.answer("Вы не админ бота.")
+        logging.info(f"Отчёт по чатам НЕ отправлен пользователю {msg.chat.id}. Так как он не админ")
+        
+
+# Колбэк для намерения удалить контекст общения с ИИ
+@router.message(F.text == message_texts.commands['delete_context'])
+async def delete_context(msg: Message):
+    is_admin = await utils.is_admin(user_id=msg.from_user.id)
+    if is_admin: 
+        trash_context_kb = [[InlineKeyboardButton(text="🚮 Удалить у себя", callback_data="delete_self_context")], 
+                        [InlineKeyboardButton(text="🚮 Удалить у всех ❗", callback_data="delete_all_context")]]
+        trash_context_kb = InlineKeyboardMarkup(inline_keyboard=trash_context_kb)
+            # Вывод ответа администратору
+        await msg.answer("Можете удалить контекст только у себя или у всех пользователей.", reply_markup=trash_context_kb)
+    else:
+        await msg.answer("Вы не админ бота.")
+        logging.info(f"Контекст сообщений НЕ очищен пользователю {msg.chat.id}. Так как он не админ")
+    
+        
+# Обработка текстовых сообщений пользователей
+
+@router.message(F.content_type == 'text' )
 async def chat_with_client(msg: Message):
     """
     Bot actions in chat
     """
+    if msg.text == env('ADMIN_TOKEN'):
+        await utils.make_admin(msg.from_user.id)
+        admin_kb = [[KeyboardButton(text=message_texts.commands['report'], callback_data="get_report")], 
+                    [KeyboardButton(text=message_texts.commands['delete_context'], callback_data="delete_context")]]
+        admin_kb = ReplyKeyboardMarkup(keyboard=admin_kb, resize_keyboard=True)
+         # Вывод ответа администратору
+        await msg.answer(f"Пользователь {msg.from_user.id} успешно добавлен в администраторы.", reply_markup=admin_kb)
+        return None
+    #Добавляем пользователя в БД, есои его нет
     await utils.insert_user(msg.from_user.id, msg.from_user.username)
     conversation = await utils.get_user_conversation(msg.chat.id)
     input = msg.text
+    logging.info(f"Обработка запроса пользователя {msg.from_user.id}. Запрос: {input}")
     await bot.send_chat_action(chat_id=msg.chat.id, action="typing")
+    # Получаем ответ от ОпенАИ
     answer = await utils.get_answer_from_llm(conversation, input)
+    logging.info(f"Ответ на запрос пользователя {msg.from_user.id}. Ответ от ИИ: {answer}")
     # Сохраняем сообщения в базе
     await utils.insert_chat(msg.from_user.id, input=input, output=answer)
-    await msg.answer(
-        answer
-    )  # Greeting message to user
-    # logging.info(utils.convesations)
-    logging.info(input)
-    logging.info(answer)
+    # Проверка является ли пользователь админом
+    is_admin = await utils.is_admin(user_id=msg.from_user.id)
+    if is_admin:        
+        admin_kb = [[KeyboardButton(text=message_texts.commands['report'], callback_data="get_report")], 
+                    [KeyboardButton(text=message_texts.commands['delete_context'], callback_data="delete_context")]]
+        admin_kb = ReplyKeyboardMarkup(keyboard=admin_kb, resize_keyboard=True)
+         # Вывод ответа администратору
+        await msg.answer(
+        answer, reply_markup=admin_kb
+    ) 
+    else:
+        # Вывод ответа обчному пользователю
+        await msg.answer(answer)  
     
+    # logging.info(input)
+    # logging.info(answer)
     
+
+# Обработка сообщений с прикрепленным файлом
 @router.message(F.content_type == 'document')
 async def load_files(msg : Message, state: FSMContext):
-    if msg.document:
-        file_id = msg.document.file_id
-        file_info = await bot.get_file(file_id)
-        file_path = file_info.file_path
-        file_name = msg.document.file_name
-        # Download the file
-        file = await bot.download_file(file_path)
-        print('Take file:', file_name)
-        try:
-            print('Parsing..')
-            parse_token = file_name.split('.')[0]
-            print('Parsed_token:', parse_token)
-            if parse_token == env('TOKEN_SYSTEM_PROMPT'):
-                with open(f'docs/system_prompt.txt', 'wb') as new_file:
-                    new_file.write(file.read())
-                await msg.reply("Обновлен системный промпт. Модель переобучается...")
-            elif parse_token == env('TOKEN_TRAIN_SCRIPT'):
-                with open(f'docs/sales_scripts.xlsx', 'wb') as new_file:
-                    new_file.write(file.read())
-                await msg.reply("Обновлен скрипт продаж. Модель переобучается...")
-            else:
-                await msg.reply("Я не могу принимать документы.")
-        except:
-            await msg.reply("Неизвестный формат документа")
+    is_admin = await utils.is_admin(user_id=msg.from_user.id)
+    if is_admin:
+        if msg.document:
+            file_id = msg.document.file_id
+            file_info = await bot.get_file(file_id)
+            file_path = file_info.file_path
+            file_name = msg.document.file_name
+            # Download the file
+            file = await bot.download_file(file_path)
+            print('Take file:', file_name)
+            try:
+                if file_name == 'system_prompt.txt':
+                    with open(f'docs/system_prompt.txt', 'wb') as new_file:
+                        new_file.write(file.read())
+                    await asyncio.sleep(1)
+                    await msg.reply("Обновлен системный промпт. Модель переобучится в скором времени...")
+                    logging.info(f"Обновлен системный промпт пользователем {msg.from_user.id}")
+                elif file_name == 'sales_scripts.xlsx':
+                    with open(f'docs/sales_scripts.xlsx', 'wb') as new_file:
+                        new_file.write(file.read())
+                    await asyncio.sleep(1) 
+                    await msg.reply("Обновлен скрипт продаж. Модель переобучится в скором времени...")
+                    logging.info(f"Обновлен скрипт продаж пользователем {msg.from_user.id}")
+                else:
+                    await msg.reply("Я принимаю только магические документы...")
+            except:
+                await msg.reply("Неизвестный формат документа")
     else:
         await msg.reply("Я не могу принимать документы.")
 
+
+
+
+    
+
+# Колбэк для удаления контекста общения ИИ только у себя
+@router.callback_query(F.data == "delete_self_context")
+async def delete_self_context(clbck: CallbackQuery, state: FSMContext):
+    await utils.delete_self_context(clbck.message.chat.id)
+    await clbck.message.answer("Контекст Вашего общения с ИИ удален") 
+
+
+
+# Колбэк для удаления контекста общения ИИ только у себя
+@router.callback_query(F.data == "delete_all_context")
+async def delete_self_context(clbck: CallbackQuery, state: FSMContext):
+    await utils.delete_all_context(clbck.message.chat.id)
+    await clbck.message.answer("Контекст общения с ИИ всех пользователей очищен") 
--- bot/utils.py
+++ bot/utils.py
@@ -46,20 +46,14 @@ class CustomConversationTokenBufferMemory(ConversationTokenBufferMemory):
         d = super().load_memory_variables(inputs)
         d.update({k:inputs.get(k) for k in self.extra_variables})        
         return d
-    
-                                 
-system_prompt = ''
-scripts_text = ''
 
 
+async def update_info() -> tuple[str, str]:
+    """ Обновление системного промпта и скрипта продаж
 
-
-    
-
-async def update_info():
-    '''
-    Обновление системного промпта и скрипта продаж
-    '''
+    Returns:
+        tuple[str, str]: системный промпт, скрипт продаж
+    """   
 
     excel_script_path = 'docs/sales_scripts.xlsx'
     wb = openpyxl.load_workbook(excel_script_path)
@@ -69,30 +63,35 @@ async def update_info():
         scripts_text += "Клиент:" + str(row[0].value) + '\n' + "AI ассистент:" + str(row[1].value) + '\n'
     scripts_text 
     
-    print('Считан скрипт продаж:\n', scripts_text)
+    # print('Считан скрипт продаж:\n', scripts_text)
     # with open('docs/train_script.txt', 'r') as f:
     #     scripts_text = f.read()
     
     with open('docs/system_prompt.txt', 'r') as f:
         system_prompt = f.read()
-    print('Считан системный промпт\n', system_prompt)
+    #print('Считан системный промпт\n', system_prompt)
     
     return system_prompt, scripts_text
 
 
-async def get_prompt():
-        system_prompt, scripts_text = await update_info()
-        template = f"""{system_prompt}\n
-        Скрипт продаж:\n {scripts_text}\n"""
-        template += """История общения с текущим клиентом: \n
-        {history}\n
-        Клиент: \n
-        {input} \n
-        AI ассистент:\n"""
-        prompt_template = PromptTemplate(input_variables= ["history", "input"],
-            template=template,)
-        
-        return prompt_template
+async def get_prompt() -> PromptTemplate:
+    """Сборка шаблона промпта
+
+    Returns:
+        PromptTemplate: Шаблон промпта
+    """        
+    system_prompt, scripts_text = await update_info()
+    template = f"""{system_prompt}\n
+    Скрипт продаж:\n {scripts_text}\n"""
+    template += """История общения с текущим клиентом: \n
+    {history}\n
+    Клиент: \n
+    {input} \n
+    AI ассистент:\n"""
+    prompt_template = PromptTemplate(input_variables= ["history", "input"],
+        template=template,)
+    
+    return prompt_template
         
 # async def get_conversation_chain_history(prompt_template, verbose=False):
 
@@ -120,27 +119,68 @@ async def get_prompt():
 #     return conversation
 
 
-async def get_user_conversation(user_id, verbose=False):
+async def get_user_conversation(user_id: int, verbose=False) -> ConversationChain:
+    """Возвращает цепочку общения
+
+    Args:
+        user_id (int): телеграм ID пользователя
+        verbose (bool, optional): Выводить в принт всю цепочку общения или нет. Defaults to False.
+
+    Returns:
+        ConversationChain: Цепочка общения
+    """    
     prompt_template = await get_prompt()
     llm = ChatOpenAI(openai_api_key=env("OPENAI_TOKEN"), temperature=0.7)
     conversation = ConversationChain(
     llm=llm, verbose=verbose, prompt=prompt_template, memory=CustomConversationTokenBufferMemory(llm=llm, max_token_limit=2000, human_prefix="Клиент", ai_prefix="AI ассистент"))
-    connection = sqlite3.connect('seller_bot.db')
-    cursor = connection.cursor()        
-    # Извлекаем историю сообщений пользователя
-    cursor.execute('SELECT input, output FROM (SELECT chat_id, input, output FROM chats WHERE user_id = ? ORDER BY chat_id DESC LIMIT 10) ORDER BY chat_id', (user_id, ))
-    user_history= cursor.fetchall()
+    try:    
+        connection = sqlite3.connect('seller_bot.db')
+        cursor = connection.cursor()        
+        # Извлекаем историю сообщений пользователя
+        cursor.execute('SELECT input, output FROM (SELECT chat_id, input, output FROM chats WHERE user_id = ? and in_context = 1 ORDER BY chat_id DESC LIMIT 10) ORDER BY chat_id', (user_id, ))
+        user_history= cursor.fetchall()
+    except sqlite3.Error as error:
+        logging.info("Ошибка при работе с SQLite", error)
+    finally:
+        if connection:
+            connection.close()
+            logging.info(f"Выгрузка чата с пользователем {user_id}. Соединение с SQLite закрыто")
     if user_history:
-        _ = [conversation.memory.save_context({"input": input }, {"output": output}) for input, output in user_history]    
+        for input, output in user_history:
+            conversation.memory.save_context({"input": input }, {"output": output}) # Заполнение цепочки сообщения контекстом общения с пользователем 
     return conversation
     
-async def get_answer_from_llm(conversation, input):
-    prompt_template = await get_prompt()
-    answer = await conversation.ainvoke(input=input)
-    return answer['response']
+    
+async def get_answer_from_llm(conversation: ConversationChain, input: str) -> str:
+    """Получение ответов от OpenAI модели GPT
+
+    Args:
+        conversation (ConversationChain): Цепочка общения
+        input (str): запрос пользователя
+
+    Returns:
+        str: ответ ИИ
+    """
+    try:    
+        answer = await conversation.ainvoke(input=input)
+        return answer['response']
+    except:
+        logging.error('Ошибка в получении ответа от ИИ')
+        return None
 
 
-async def insert_chat(user_id, input, output):
+async def insert_chat(user_id: int, input: str, output: str) -> bool:
+    """Добавление текущего чата (вопрос-ответ) в БД
+
+    Args:
+        user_id (int): телеграм ID пользователя
+        input (str): вопрос пользователя
+        output (str): ответ ИИ ассистента
+
+    Returns:
+        bool: Статус выполнения функции
+    """    
+    try:
         connection = sqlite3.connect('seller_bot.db')
         cursor = connection.cursor()
 
@@ -150,9 +190,28 @@ async def insert_chat(user_id, input, output):
         # Сохраняем изменения и закрываем соединение
         connection.commit()
         connection.close()
+        status = True
+    except sqlite3.Error as error:
+        logging.info("Ошибка при работе с SQLite", error)
+        status = False
+    finally:
+        if connection:
+            connection.close()
+            logging.info("Чат с пользователем {user_id} обновлен в БД.Соединение с SQLite закрыто")
+        return status
         
 
-async def insert_user(user_id, username):
+async def insert_user(user_id: int, username: str) -> bool:
+    """Добавление нового пользователя в БД
+
+    Args:
+        user_id (int): телеграм ID пользователя
+        username (str): username пользователя
+
+    Returns:
+        bool: статус выполнения функции
+    """    
+    try:
         connection = sqlite3.connect('seller_bot.db')
         cursor = connection.cursor()        
         # Проверяем наличие пользователя
@@ -162,6 +221,150 @@ async def insert_user(user_id, username):
         # Добавляем нового пользователя
             cursor.execute('INSERT INTO users (user_id, username) VALUES (?, ?)', (user_id, username))
         # Сохраняем изменения и закрываем соединение
+            connection.commit()
+        status = True
+    except sqlite3.Error as error:
+        logging.info("Ошибка при работе с SQLite", error)
+        status = False
+    finally:
+        if connection:
+            connection.close()
+            logging.info(f"Пользователь {user_id} добавлен в БД. Соединение с SQLite закрыто")
+        return status
+        
+        
+async def is_admin(user_id: int) -> bool:
+    """Проверяет является ли пользователь администратором бота
+
+    Args:
+        user_id (int): телеграм ID пользователя 
+
+    Returns:
+        bool: Является ли пользователь админом
+    """
+    try:           
+        connection = sqlite3.connect('seller_bot.db')
+        cursor = connection.cursor()        
+        # Проверяем наличие пользователя
+        cursor.execute('SELECT user_id FROM users WHERE user_id = ? and isadmin = 1', (user_id, ))
+        is_admin = cursor.fetchall()
         connection.commit()
         connection.close()
+    except sqlite3.Error as error:
+        logging.info("Ошибка при работе с SQLite", error)
+    finally:
+        if connection:
+            connection.close()
+            logging.info(f"Проверка наличия пользователя {user_id} в админах. Соединение с SQLite закрыто")
+    if is_admin == []:
+        return False
+    else:
+        return True
+
+
+async def make_admin(user_id: int) -> bool:
+    """Включение пользователя в списки администраторов телеграм бота
+
+    Args:
+        user_id (int): телеграм ID пользователя 
+
+    Returns:
+        bool: Успешно добавлен
+    """
+    try:
+        connection = sqlite3.connect('seller_bot.db')
+        cursor = connection.cursor()        
+        # Проверяем наличие пользователя
+        cursor.execute('UPDATE users SET isadmin = 1 WHERE user_id = ?', (user_id, ))
+        connection.commit()
+        status = True
+    except sqlite3.Error as error:
+        logging.info("Ошибка при работе с SQLite", error)
+        status = False
+    finally:
+        if connection:
+            connection.close()
+            logging.info(f"Пользователь {user_id} включен в списки админов. Соединение с SQLite закрыто")
+        return status
+            
+
+async def create_chats_report():
+    """Формирует выгрузку с БД по чатам с клиентами для отправки админу
+    """
+    chat_report = None
+    try:
+        connection = sqlite3.connect('seller_bot.db')
+        cursor = connection.cursor()        
+        # Проверяем наличие пользователя
+        cursor.execute('SELECT chat_datetime, chat_id, input, output, chats.user_id, username, isadmin FROM chats LEFT JOIN users ON chats.user_id = users.user_id;')
+        chat_report = cursor.fetchall()
+    except sqlite3.Error as error:
+        logging.info("Ошибка при работе с SQLite", error)
+    finally:
+        if connection:
+            connection.close()
+            logging.info(f"Отчет по чатам выгружен. Соединение с SQLite закрыто")
+            import openpyxl
+            wb = openpyxl.Workbook()
+            sheet = wb.active
+            sheet.cell(row=1,column=1).value= 'datetime'
+            sheet.cell(row=1,column=2).value = 'chat_id'
+            sheet.cell(row=1,column=3).value = 'user_message'
+            sheet.cell(row=1,column=4).value = 'ai_message'
+            sheet.cell(row=1,column=5).value = 'user_id'
+            sheet.cell(row=1,column=6).value = 'username'
+            sheet.cell(row=1,column=7).value = 'is_admin'
+            for enum, values in enumerate(chat_report):
+                for col, value in enumerate(values):
+                    sheet.cell(row=enum+2,column=col+1).value = value
+            wb.save('docs/chat_history.xlsx')
+            return chat_report
         
+        
+async def delete_self_context(user_id: int) -> bool:
+    """
+    Удаление контекста общения с ИИ у одного пользователя
+    Args:
+        user_id (int): телеграм ID пользователя 
+    Returns:
+        bool: Результат выполнения
+    """
+    try:
+        connection = sqlite3.connect('seller_bot.db')
+        cursor = connection.cursor()        
+        # Проверяем наличие пользователя
+        cursor.execute('UPDATE chats SET in_context = 0 WHERE user_id = ?', (user_id, ))
+        connection.commit()
+        state = True
+    except sqlite3.Error as error:
+        logging.info("Ошибка при работе с SQLite", error)
+        state = False
+    finally:
+        if connection:
+            connection.close()
+            logging.info(f"Контекст общения с пользователем {user_id} очищен. Соединение с SQLite закрыто")
+        return state
+            
+            
+async def delete_all_context() -> bool:
+    """
+    Удаление контекста общения с ИИ у одного пользователя
+
+    Returns:
+        bool: Результат выполнения
+    """
+    try:
+        connection = sqlite3.connect('seller_bot.db')
+        cursor = connection.cursor()        
+        # Проверяем наличие пользователя
+        cursor.execute('UPDATE chats SET in_context = 0')
+        connection.commit()
+        status = True
+    except sqlite3.Error as error:
+        logging.info("Ошибка при работе с SQLite", error)
+        status = True
+    finally:
+        if connection:
+            connection.close()
+            logging.info(f"Контекст общения со всеми пользователями очищен. Соединение с SQLite закрыто")  
+        return status     
